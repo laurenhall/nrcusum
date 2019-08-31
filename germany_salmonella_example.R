6ba16f8067c89c6e256d2fb6c3987b4c0fb72013
laurenhall <41134932+laurenhall@users.noreply.github.com>
2019-08-30 21:56:53 -0600

    Add files via upload

--- germany_salmonella_example.R
+++ germany_salmonella_example.R
@@ -9,42 +9,44 @@ data(salmNewport)
 # Isolate the observed counts
 dat = observed(salmNewport); dim(dat)
 
-# Plot the total cases over time to isolate the outbreak we want to detect
-#pdf(file="snewportcases.pdf", width=7, height=5)
+# Figure 6 - Total S. Newport cases over time. 
 setEPS()
-postscript(file="snewportcases.eps", width=7, height=5, bg="white")
-plot(apply(dat, 1, sum), type="l", xlab="Time (in Weeks)", 
+postscript(file="Fig6.eps", width=7, height=5, bg="white")
+plot(apply(dat, 1, sum), type="l", xlab="Time (in Weeks)",
      ylab="Total Cases", main="")
 dev.off()
+
 # The outbreak we want to detect is the large spike from about t=408 to t=410.
-# Region 12 was the only region not to report any cases of S. Newport between weeks 400 and 415, and so can be excluded.
-dat = dat[,-12] 
-# Estimate the in-control mean for each region using three years of data. 
-lambda = sapply(1:15, function(x) mean(dat[1:156,x])) 
-# Run the CUSUM on 15 states that recorded cases during the outbreak. 
-testn = pblapply(1:15, function(x) {set.seed(x); nrc.csave(dat[,x], lambda[x], lambda[x]*1.5, correction="n", sig=0.05, nsim = 1999)})
-tests = pblapply(1:15, function(x) {set.seed(x); nrc.csave(dat[,x], lambda[x], lambda[x]*1.5, correction="s", sig=0.05, nsim = 1999)})
-teste = pblapply(1:15, function(x) {set.seed(x); nrc.csave(dat[,x], lambda[x], lambda[x]*1.5, correction="e", sig=0.05, nsim = 1999)})
-testb = pblapply(1:15, function(x) {set.seed(x); nrc.csave(dat[,x], lambda[x], lambda[x]*1.5, correction="b", sig=0.05, nsim = 1999)})
+# Region 12 was the only region not to report any cases of S. Newport between weeks 400 and 415, and so is excluded from the analysis.
+dat = dat[,-12]
 
+# Estimate the in-control mean for each region using three years of data.
+lambdas = sapply(1:15, function(x) mean(dat[1:156,x]))
+
+# Run the CUSUM on 15 states that recorded cases during the outbreak.
+test_n = pblapply(1:15, function(x) {set.seed(x); csm_corrected(dat[,x], lambdas[x], lambdas[x]*1.5, correction="n", sig=0.05, nsim = 1999)})
+test_s = pblapply(1:15, function(x) {set.seed(x); csm_corrected(dat[,x], lambdas[x], lambdas[x]*1.5, correction="s", sig=0.05, nsim = 1999)})
+test_e = pblapply(1:15, function(x) {set.seed(x); csm_corrected(dat[,x], lambdas[x], lambdas[x]*1.5, correction="e", sig=0.05, nsim = 1999)})
+test_b = pblapply(1:15, function(x) {set.seed(x); csm_corrected(dat[,x], lambdas[x], lambdas[x]*1.5, correction="b", sig=0.05, nsim = 1999)})
+
+
+firstalarm = alarms = matrix(NA, nrow=15, ncol=4, dimnames = list(colnames(dat), c("No Correction","Lambda[1]", "Estimation", "Bootstrap")))
 # Time of first alarm after outbreak
-firstalarm = matrix(NA, nrow=15, ncol=4); rownames(firstalarm) = colnames(dat); colnames(firstalarm) = c("No Correction","Lambda to Detect", "Estimation", "Bootstrap")
 for(i in 1:15){
-  firstalarm[i,1] = (408:528)[which(testn[[i]]$decision[408:528] == 1)][1]
-  firstalarm[i,2] = (408:528)[which(tests[[i]]$decision[408:528] == 1)][1]
-  firstalarm[i,3] = (408:528)[which(teste[[i]]$decision[408:528] == 1)][1]
-  firstalarm[i,4] = (408:528)[which(testb[[i]]$decision[408:528] == 1)][1]
+  firstalarm[i,1] = (408:528)[test_n[[i]]$decision[408:528] == 1][1]
+  firstalarm[i,2] = (408:528)[test_s[[i]]$decision[408:528] == 1][1]
+  firstalarm[i,3] = (408:528)[test_e[[i]]$decision[408:528] == 1][1]
+  firstalarm[i,4] = (408:528)[test_b[[i]]$decision[408:528] == 1][1]
 }
 
-firstalarm
+print(firstalarm)
 
-# calculate number of alarms after detection: total alarms from 412
-alarms = matrix(NA, nrow=15, ncol=4); rownames(alarms) = colnames(dat); colnames(alarms) = c("No Correction","Lambda to Detect", "Estimation", "Bootstrap") 
+# Calculate the number of alarms that occured between week 412 and the end of the study period.  
 for(i in 1:15){
-  alarms[i,1] = max(0,sum(which(testn[[i]]$decision == 1) > 412))
-  alarms[i,2] = max(0,sum(which(tests[[i]]$decision == 1) > 412))
-  alarms[i,3] = max(0,sum(which(teste[[i]]$decision == 1) > 412))
-  alarms[i,4] = max(0,sum(which(testb[[i]]$decision == 1) > 412))
+  alarms[i,1] = sum(which(test_n[[i]]$decision == 1) > 412)
+  alarms[i,2] = sum(which(test_s[[i]]$decision == 1) > 412)
+  alarms[i,3] = sum(which(test_e[[i]]$decision == 1) > 412)
+  alarms[i,4] = sum(which(test_b[[i]]$decision == 1) > 412)
 }
 
-alarms
+print(alarms)
